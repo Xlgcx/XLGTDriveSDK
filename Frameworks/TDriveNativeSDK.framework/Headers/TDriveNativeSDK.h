5a5244704c11c5226efff8d60832852352be215c
韩占禀 <106414958+Xlgcx@users.noreply.github.com>
2022-10-18 15:57:58 +0800

    Update TDriveNativeSDK.h

--- Frameworks/TDriveNativeSDK.framework/Headers/TDriveNativeSDK.h
+++ Frameworks/TDriveNativeSDK.framework/Headers/TDriveNativeSDK.h
@@ -10,16 +10,16 @@
 #import <TDriveNativeSDK/TDriverSDK.h>
 #import <TDriveNativeSDK/TDMonthDataVC.h>
 
-//#pragma mark - 试驾小助引用
-//#import <TDriveNativeSDK/DPAudioRecorder.h>
-//#import <TDriveNativeSDK/XLGRecordUtils.h>
-//#import <TDriveNativeSDK/JX_GCDTimerManager.h>
-//#import <TDriveNativeSDK/XLGRecordModel.h>
-//#import <TDriveNativeSDK/JQFMDB.h>
-//#import <TDriveNativeSDK/ADAddressUtils.h>
-//#import <TDriveNativeSDK/KeyDefine.h>
-//#import <TDriveNativeSDK/TDNativeUtils.h>
-//#import <TDriveNativeSDK/URLDefine.h>
-//#import <TDriveNativeSDK/TDNativeNetworkUtil.h>
-//#import <TDriveNativeSDK/TDNativeFlutterManager.h>
+#pragma mark - 试驾小助引用
+#import <TDriveNativeSDK/DPAudioRecorder.h>
+#import <TDriveNativeSDK/XLGRecordUtils.h>
+#import <TDriveNativeSDK/JX_GCDTimerManager.h>
+#import <TDriveNativeSDK/XLGRecordModel.h>
+#import <TDriveNativeSDK/JQFMDB.h>
+#import <TDriveNativeSDK/ADAddressUtils.h>
+#import <TDriveNativeSDK/KeyDefine.h>
+#import <TDriveNativeSDK/TDNativeUtils.h>
+#import <TDriveNativeSDK/URLDefine.h>
+#import <TDriveNativeSDK/TDNativeNetworkUtil.h>
+#import <TDriveNativeSDK/TDNativeFlutterManager.h>
 
